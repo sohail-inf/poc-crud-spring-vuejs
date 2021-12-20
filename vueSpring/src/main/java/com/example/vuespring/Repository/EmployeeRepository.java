e94e3b303cced8ff58661216d7cc3763950ece68
sohail-inf <sohail.inflack@bkash.com>
2021-12-20 13:14:44 +0600

    modified

--- vueSpring/src/main/java/com/example/vuespring/Repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.vuespring.Repository;
-
-import com.example.vuespring.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
